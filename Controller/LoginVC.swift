a06d5ae8a3aa12fb9ca3066d8926a1db4ab0df48
Dick Jan Broer <djb902@dicks-imac.home>
2018-07-23 13:40:00 +0200

    lesson 4

---
+++ Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Dick Jan Broer on 23/07/2018.
+//  Copyright © 2018 Dick Jan Broer. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Utilities/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Dick Jan Broer on 23/07/2018.
+//  Copyright © 2018 Dick Jan Broer. All rights reserved.
+//
+
+import Foundation
+let TO_LOGIN = "toLogin"
