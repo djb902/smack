b34df1ad602c143df2701a586b2c84e99cfdad29
Dick Jan Broer <djb902@dicks-imac-2.home>
2018-07-25 14:37:45 +0200

    lesson9

--- Utilities/Constants.swift
+++ Utilities/Constants.swift
@@ -8,7 +8,22 @@
 
 import Foundation
 
+//RENAME A CLOSURE
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// Userdefaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+// url constants
+//TEST
+let BASE_URL = "http://localhost:3005/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+//PRD
+//let BASE_URL: "https://chattychatchatdjb2.herokuapp.com/v1"
